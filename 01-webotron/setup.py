59481bf9c4da9a532141180682e8dea834a15ac8
er.ankit.cs <er.ankit.cs@gmail.com>
2020-02-08 16:31:45 +1100

    Updated README

--- 01-webotron/setup.py
+++ 01-webotron/setup.py
@@ -1,15 +1,23 @@
 from setuptools import setup
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='webotron-20',
-    version='0.2',
+    version='0.4',
     author='Ankit K Singh',
     author_email='er.ankit.cs@gmail.com',
     description='Webotron 20 is a tool to deploy static website to AWS.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     license='MIT',
     packages=['webotron'],
     url='https://github.com/erankitcs/automating-aws-with-python',
-    download_url='https://github.com/erankitcs/automating-aws-with-python/archive/v0.2.tar.gz',
+    download_url='https://github.com/erankitcs/automating-aws-with-python/archive/v0.4.tar.gz',
     keywords = ['AWS', 'BOTO3', 'S3Website','CloudFront'],
     install_requires=[
         'click',
