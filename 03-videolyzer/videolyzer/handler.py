2757524e89224b3d8d6cf373ef9403a03663be44
er.ankit.cs <er.ankit.cs@gmail.com>
2020-03-27 20:51:28 +1100

    Integrating Lambda with Rekognition label detection.

--- 03-videolyzer/videolyzer/handler.py
+++ 03-videolyzer/videolyzer/handler.py
@@ -1,4 +1,28 @@
+import urllib
+import boto3
+
+def start_label_detection(bucket, key):
+    rekognition_client = boto3.client('rekognition')
+    print('Starting Label Detection for bucket: {} and Video Name: {}'.format(bucket,key))
+    response = rekognition_client.start_label_detection(
+        Video={
+            'S3Object':{
+                'Bucket' : bucket,
+                'Name' : key
+            }
+        }
+    )
+    print(response)
+    return
+
 def start_processing_video(event, context):
-    print(event)
-    print("event completed.")
+    print("Event Recieved..")
+    for record in event['Records']:
+        bucket = record['s3']['bucket']['name']
+        key = record['s3']['object']['key']
+        start_label_detection(
+            bucket,
+            urllib.parse.unquote_plus(key)
+        )
+
     return
