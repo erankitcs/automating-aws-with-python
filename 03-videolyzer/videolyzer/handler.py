476d5a0c6e3964c4e55354d777442a791177308e
er.ankit.cs <er.ankit.cs@gmail.com>
2020-03-28 14:58:39 +1100

    Integrating DynamoDB with rekognition to save detected labels.

--- 03-videolyzer/videolyzer/handler.py
+++ 03-videolyzer/videolyzer/handler.py
@@ -1,14 +1,10 @@
 import os
 import urllib
 import boto3
+import json
 
 def start_label_detection(bucket, key):
     rekognition_client = boto3.client('rekognition')
-    print('Starting Label Detection for bucket: {} and Video Name: {}'.format(bucket,key))
-    print('SNS ARN')
-    print(os.environ['REKOGNITION_SNS_TOPIC_ARN'])
-    print('SNS ROLE ARN')
-    print(os.environ['REKOGNITION_ROLE_ARN'])
     response = rekognition_client.start_label_detection(
         Video={
             'S3Object':{
@@ -21,11 +17,9 @@ def start_label_detection(bucket, key):
             'RoleArn': os.environ['REKOGNITION_ROLE_ARN']
         }
     )
-    print(response)
     return
 
 def start_processing_video(event, context):
-    print("Event Recieved..")
     for record in event['Records']:
         bucket = record['s3']['bucket']['name']
         key = record['s3']['object']['key']
@@ -36,7 +30,53 @@ def start_processing_video(event, context):
 
     return
 
+def get_video_labels(job_id):
+    rekognition_client = boto3.client('rekognition')
+    response = rekognition_client.get_label_detection(JobId=job_id)
+    next_token = response.get('NextToken', None)
+    while next_token:
+        next_page = rekognition_client.get_label_detection(
+            JobId=job_id,
+            NextToken=next_token
+        )
+        next_token=next_page.get('NextToken',None)
+        response['Labels'].extend(next_page['Labels'])
+    return response
+
+def make_item(data):
+
+    if isinstance(data, dict):
+        return { k: make_item(v) for k , v in data.items() }
+
+    if isinstance(data, list):
+        return [ make_item(v) for v in data ]
+
+    if isinstance(data, float):
+        return str(data)
+
+    return data
+
+def put_labels_in_db(data, video_name, video_bucket):
+    del data['ResponseMetadata']
+    del data['JobStatus']
+    data['videoName'] = video_name
+    data['videoBucket'] = video_bucket
+
+    dynamodb = boto3.resource('dynamodb')
+    table_name = os.environ['DYNAMODB_TABLE_NAME']
+
+    videos_table = dynamodb.Table(table_name)
+    data = make_item(data)
+    videos_table.put_item(Item=data)
+
+
 def handle_label_detection(event, context):
-    print('Handle Label Detection invoked.')
-    print(event)
+    for record in event['Records']:
+        message = json.loads(record['Sns']['Message'])
+        job_id = message['JobId']
+        s3_object = message['Video']['S3ObjectName']
+        s3_bucket = message['Video']['S3Bucket']
+        response = get_video_labels(job_id)
+        put_labels_in_db(response, s3_object, s3_bucket)
+
     return
